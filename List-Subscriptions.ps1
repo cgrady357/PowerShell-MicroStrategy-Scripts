4476d19f67a069bc72308cc18ab3becb40314c56
Craig Grady <cgrady357@gmail.com>
2015-12-10 09:31:45 -0600

    Changes to be committed: 	new file:   List-Subscriptions.ps1

---
+++ List-Subscriptions.ps1
@@ -0,0 +1,16 @@
+<#
+        .NOTES
+         AUTHOR....:  Craig Grady
+         LAST EDIT.:  12/09/2015
+         CREATED...:  12/09/2015
+#>
+
+[CmdletBinding()]
+param(
+[string]$LoginName,
+[string]$ProjectName
+)
+
+"LIST SUBSCRIPTIONS FOR OWNER `"$LoginName`" FOR PROJECT `"$ProjectName`";"
+		
+}		
